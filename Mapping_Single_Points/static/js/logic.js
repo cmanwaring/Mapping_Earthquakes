addaecc5ed01d86e9ecdec77949162f23ea791d2
Clay <78937867+cmanwaring@users.noreply.github.com>
2021-06-04 19:43:48 -0500

    Uploading mapping with working circle markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,10 +2,25 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 3);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// //  Add a marker to the map for NYC, NY.
+// let marker = L.marker([40.7128, -74.0060]).addTo(map);
+
+// //  Add a marker to the map for Los Angeles, California.
+// let circle = L.circle([34.0522, -118.2437], {
+//     radius: 100
+// }).addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let circle = L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
